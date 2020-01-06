9607beccd07d687280da665e8e65239e3e56a017
Lucas Anderton <lucasanderton5@gmail.com>
2020-01-05 19:29:43 -0500

    Added r4datascience tutorials for chapter 3 and a reference image for scatter plot shape codes

---
+++ Lucas/python/atbs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# this program says hello and asks for my name.
+
+print('Hello world')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ',myName)
+print("Your name is", len(myName), "letters long.")
+print("What is your age?")
+myAge = input()
+print('You will be' + str(int(myAge) + 1) + 'in a year.')
